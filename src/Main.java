0ab6affe61568ddb5535daa37f018d706ad4a615
Sabina Allahyarova <125457757+sabialhyv@users.noreply.github.com>
2023-04-19 01:34:10 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         Scanner sc = new Scanner(System.in);
+
+                int min = Integer.MAX_VALUE;
+
+                System.out.println("Enter 10 numbers:");
+                for (int i = 0; i < 10; i++) {
+                    int number = sc.nextInt();
+                    if (number % 2 == 0 && number < min) {
+                        min = number;
+                    }
+                }
+
+                if (min != Integer.MAX_VALUE) {
+                    System.out.println("Minimum even number :" + min);
+                } else {
+                    System.out.println("There are no even number!");
+                }
     }
 }
